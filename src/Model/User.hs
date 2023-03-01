4069c0af80d6c9cdbbad548d04af5ffc5a1e8253
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-01 23:45:22 +0900

    Passed insertUser test.

--- src/Model/User.hs
+++ src/Model/User.hs
@@ -6,12 +6,20 @@
 
 module Model.User (NewUser (NewUser, nuName, nuPassword), insertUser) where
 
+import Control.Exception (catch)
+import Crypto.BCrypt (hashPasswordUsingPolicy, slowerBcryptHashingPolicy)
+import Data.ByteString qualified as BS
 import Data.Functor.ProductIsomorphic
+import Data.Text (pack, unpack)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Database.HDBC
 import Database.HDBC.Query.TH (makeRelationalRecord)
+import Database.HDBC.Record qualified as DHR
 import Database.Relational
+import Database.Relational qualified as HRR
 import Entity.User qualified as User
 import GHC.Generics (Generic)
+import System.IO (hPrint, hPutStrLn, stderr)
 
 data NewUser = NewUser
   { nuName :: !String,
@@ -25,4 +33,27 @@ piNewUser :: Pi User.User NewUser
 piNewUser = NewUser |$| User.name' |*| User.password'
 
 insertUser :: IConnection c => NewUser -> c -> IO Integer
-insertUser u conn = do return 0
+insertUser u conn = do
+  mHashed <-
+    hashPasswordUsingPolicy slowerBcryptHashingPolicy $
+      enc . nuPassword $
+        u
+  case mHashed of
+    Nothing -> do
+      hPutStrLn stderr "Failed to hash password"
+      return 0
+    Just hashed -> do
+      let ins = HRR.typedInsert' defaultConfig User.tableOfUser piNewUser
+          u' = u {nuPassword = dec hashed}
+      withTransaction conn $
+        \conn' ->
+          DHR.runInsert conn' ins u'
+            `catch` \e -> do
+              hPrint stderr (e :: SqlError)
+              return 0
+
+enc :: String -> BS.ByteString
+enc = encodeUtf8 . pack
+
+dec :: BS.ByteString -> String
+dec = unpack . decodeUtf8
