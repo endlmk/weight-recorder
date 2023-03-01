2e54f545c8436d7924687625472999c2ba36688f
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-01 21:24:21 +0900

    Fix test to use empty db.

--- test/Model/UserSpec.hs
+++ test/Model/UserSpec.hs
@@ -5,6 +5,8 @@ module Model.UserSpec (spec) where
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Model.User (NewUser (NewUser), insertUser)
+import System.IO.Temp (withSystemTempFile)
+import System.Process (callCommand)
 import Test.Hspec
 
 spec :: Spec
@@ -14,8 +16,11 @@ spec =
       do
         it "inserts new user" $
           do
-            c <- connectSqlite3 "weight.db"
-            _ <- insertUser (NewUser "test" "pass") c
-            st <- prepare c "SELECT COUNT(*) FROM user"
-            _ <- execute st []
-            (fetchRow st) `shouldReturn` Just [SqlInt64 1]
+            withSystemTempFile "test.db" $
+              \path _ -> do
+                callCommand $ "sqlite3 " ++ path ++ "< data/schema.sql"
+                c <- connectSqlite3 path
+                _ <- insertUser (NewUser "test" "pass") c
+                st <- prepare c "SELECT COUNT(*) FROM user"
+                _ <- execute st []
+                fetchRow st `shouldReturn` Just [SqlInt64 1]
