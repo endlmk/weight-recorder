afdfc5ec9b919bf3aeb08565f5a0bc1c21183a2f
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-03 22:41:36 +0900

    Add User Model tests.

--- test/Model/UserSpec.hs
+++ test/Model/UserSpec.hs
@@ -39,3 +39,21 @@ spec =
                       User.id usr `shouldBe` 1
                       User.name usr `shouldBe` "test"
                     Nothing -> u `shouldNotBe` Nothing
+        it "not select none exist user" $
+          do
+            withSystemTempFile "test.db" $
+              \path _ ->
+                do
+                  callCommand $ "sqlite3 " ++ path ++ "< data/schema.sql"
+                  c <- connectSqlite3 path
+                  _ <- insertUser (NewUser "test" "pass") c
+                  selectUser "test1" "pass" c `shouldReturn` Nothing
+        it "not select worng password" $
+          do
+            withSystemTempFile "test.db" $
+              \path _ ->
+                do
+                  callCommand $ "sqlite3 " ++ path ++ "< data/schema.sql"
+                  c <- connectSqlite3 path
+                  _ <- insertUser (NewUser "test" "pass") c
+                  selectUser "test1" "aaaaa" c `shouldReturn` Nothing
