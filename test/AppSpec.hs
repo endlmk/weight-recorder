795e5bdc677e0e57089cdf9cc74606066c73f120
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-05 23:39:02 +0900

    Add test with Network.Wai.Test.

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -3,6 +3,7 @@
 module AppSpec (spec) where
 
 import App (app)
+import Network.Wai.Test
 import Test.Hspec
 import Test.Hspec.Wai
 import Web.Spock (spockAsApp)
@@ -12,15 +13,24 @@ import Web.Spock (spockAsApp)
 
 spec :: Spec
 spec =
-  with (spockAsApp app) $
-    do
-      describe "GET /" $
+  do
+    describe "GET /" $
+      with (spockAsApp app) $
         do
           it "serves the home page" $
             get "/" `shouldRespondWith` "Hello World!" {matchStatus = 200}
-      describe "Get /hello/:name" $
+    describe "Get /hello/:name" $
+      with (spockAsApp app) $
         do
           it "returns hello to spock" $
             get "/hello/spock" `shouldRespondWith` "Hello spock, you are visitor number 1"
           it "returns hello to uhura" $
             get "/hello/uhura" `shouldRespondWith` "Hello uhura, you are visitor number 1"
+    describe "GET with Network.WAi.Test" $
+      it "Get home" $
+        do
+          appl <- spockAsApp app
+          flip runSession appl $
+            do
+              response <- Network.Wai.Test.request (setPath defaultRequest "/")
+              assertStatus 200 response
