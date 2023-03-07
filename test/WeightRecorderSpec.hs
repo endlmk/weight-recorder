23ef661de720c9dd9b802ad27015d69f8b4ee9ed
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-07 21:58:57 +0900

    Add Hspec.Wai test, rename spec.

---
+++ test/WeightRecorderSpec.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module WeightRecorderSpec (spec) where
+
+import Data.ByteString.Lazy.UTF8 qualified as LBS
+import Network.Wai.Test
+import Paths_weight_recorder (getDataDir)
+import System.FilePath ((</>))
+import Test.Hspec
+import Test.Hspec.Wai
+import Web.Core (WRConfig (WRConfig, wrcTplRoots))
+import Web.Spock (spockAsApp)
+import Web.WeightRecorder (weightRecorderMiddleware)
+
+-- main :: IO ()
+-- main = hspec spec
+
+spec :: Spec
+spec =
+  do
+    describe "GET /"
+      $ with
+        ( do
+            dataDir <- getDataDir
+            let cfg = WRConfig {wrcTplRoots = [dataDir </> "templates"]}
+            spockAsApp (weightRecorderMiddleware cfg)
+        )
+      $ do
+        it "serves the home page" $
+          get "/" `shouldRespondWith` 200
+    describe "GET with Network.WAi.Test" $
+      it "Get home" $
+        do
+          dataDir <- getDataDir
+          let cfg = WRConfig {wrcTplRoots = [dataDir </> "templates"]}
+              m = weightRecorderMiddleware cfg
+          appl <- spockAsApp $ m
+          flip runSession appl $
+            do
+              response <- Network.Wai.Test.request (setPath defaultRequest "/")
+              assertStatus 200 response
+              assertBodyContains (LBS.fromString "ユーザー登録") response
+              assertBodyContains (LBS.fromString "ログイン") response
