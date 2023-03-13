7acb21037a79a62946b15c35f7de1905c181fdb6
endlmk <1269467+endlmk@users.noreply.github.com>
2023-03-13 22:42:43 +0900

    Impl graph view.

--- src/Web/View/Main.hs
+++ src/Web/View/Main.hs
@@ -3,14 +3,17 @@
 module Web.View.Main where
 
 import Control.Monad.IO.Class (liftIO)
+import Data.Function (on)
+import Data.List (groupBy, sortBy)
 import Data.Text qualified as TXT
 import Data.Time qualified as TM
+import Data.Time.Format.ISO8601 (zonedTimeFormat)
 import Entity.User qualified as User
 import Entity.WeightRecord qualified as WRecord
 import Model.WeightRecord (selectWRecord)
 import Text.Mustache (Template, automaticCompile, object, substitute, (~>))
 import Text.Mustache.Types (Value)
-import Web.Core (WRAction, WRConfig (wrcTplRoots), WRState (wrstMainTemplate), runSqlite, wrconUser)
+import Web.Core (WRAction, WRConfig (wrcTplRoots), WRSession (wrsesUser), WRState (wrstMainTemplate), runSqlite, wrconUser)
 import Web.Spock (getContext, getState, html)
 
 loadMainTemplate :: WRConfig -> IO Template
@@ -27,14 +30,15 @@ mainView mMes =
     case u of
       Just user -> do
         uv <- userValue user
-        tpl <- wrstMainTemplate <$> getState
         rs <- runSqlite $ selectWRecord (User.id user)
         rvs <- mapM weightRecordValue rs
+        wgv <- weightGraphValues rs
+        tpl <- wrstMainTemplate <$> getState
         let v =
               object $
                 appendMessage
                   mMes
-                  ["user" ~> uv, "records" ~> rvs]
+                  ["user" ~> uv, "records" ~> rvs, "graphs" ~> wgv]
         html $ substitute tpl v
         where
           appendMessage (Just mes) ps = "message" ~> mes : ps
@@ -51,3 +55,20 @@ weightRecordValue wr = do
 
 toZonedTime :: TM.LocalTime -> IO TM.ZonedTime
 toZonedTime = TM.utcToLocalZonedTime . TM.localTimeToUTC TM.utc
+
+weightGraphValues :: [WRecord.WeightRecord] -> WRAction [Value]
+weightGraphValues wrs = do
+  flatWrs <- liftIO $ mapM flat wrs
+  let wrss = groupBy ((==) `on` fst) . sortBy (compare `on` fst) $ flatWrs
+  return $ map groupToValue wrss
+  where
+    flat wr = do
+      ztime <- toZonedTime $ WRecord.time wr
+      let ztimeStr = TM.formatTime TM.defaultTimeLocale "%m/%d" ztime
+          w = WRecord.weight wr
+      return (ztimeStr, w)
+    groupToValue gr =
+      let dt = head $ map fst gr
+          wt = avg $ map snd gr
+          avg xs = sum xs / fromIntegral (length xs)
+       in object ["day" ~> dt, "weight" ~> wt]
